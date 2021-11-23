bc7c4bcbcba1dff164c8d1a4052bef23f9b605f2
dhionas magalhães <dhionas17@hotmail.com>
2021-11-23 15:14:04 -0300

    Cadastrando Usuários no banco de dados

--- routes/usuario.js
+++ routes/usuario.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const mongoose = require('mongoose')
 require("../models/Usuario")
 const Usuario = mongoose.model("usuarios")
+const bcrypt = require("bcryptjs")
 
 router.get("/registro", (req, res) => {
     res.render("usuarios/registro")
@@ -37,6 +38,46 @@ router.post("/registro", (req, res) => {
         res.render("usuarios/registro", {erros: erros})
 
     }else{
+
+        Usuario.findOne({email: req.body.email}).lean().then((usuario) => {
+            if(usuario){
+                req.flash("error_msg", "Já existe uma conta com este e-mail no nosso sistema")
+                res.redirect("/usuarios/registro")
+            }else{
+
+                const novoUsuario = new Usuario({
+                    nome: req.body.nome,
+                    email: req.body.email,
+                    senha: req.body.senha
+                })
+
+
+                bcrypt.genSalt(10, (erro, salt) => {
+                    bcrypt.hash(novoUsuario.senha, salt, (erro, hash) => {
+                        if(erro){
+                            req.flash("error_msg", "Houve um erro durante o salvamento do usuário")
+                            res.redirect("/")
+                        }
+
+                        novoUsuario.senha = hash
+
+                        novoUsuario.save().then(() => {
+                            req.flash("success_msg", "Usuário criado com sucesso!")
+                            res.redirect("/")
+                        }).catch((err) => {
+                            req.flash("error_msg", "Houve um erro ao criar o usuário, tente novamente!")
+                            res.redirect("/usuarios/registro")
+                        })
+
+
+                    })
+                })
+
+            }
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno")
+            res.redirect("/")
+        })
 
     }
 
